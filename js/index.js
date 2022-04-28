4fc4e21da23312cf0316ee588e53598230dec48c
imzigma <eduardozigma@icloud.com>
2022-04-28 18:02:21 -0500

    Changes HomePage

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-Swal.fire(
+/* Swal.fire(
     '¡Bienvenida!',
     'Esta pagina se encuentra en construcción. <br> Búscanos en nuestras redes sociales. <br> ¡Agradecemos tu preferencia!',
     'info'
